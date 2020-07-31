184e787a79f10d955acbe81c62b44b1efe0996da
anna-184702 <45528489+anna-184702@users.noreply.github.com>
2020-07-31 14:21:59 +0100

    Update multivar_models_precip_init.py

--- convCNP/models/multivar_models_precip_init.py
+++ convCNP/models/multivar_models_precip_init.py
@@ -11,7 +11,7 @@ from .models.utils import force_positive
 class GammaElevStacked(nn.Module):
     """
     Multivariate downscaling P(Tmax|precip)
-    - Precip
+    - Precipitation
     Parameters:
     ----------
     decoder: convolutional architecture
@@ -23,7 +23,6 @@ class GammaElevStacked(nn.Module):
 
     def __init__(self, 
                  decoder,
-                 tmax_model,
                  in_channels = 1, 
                  ls = 0.1):
         super().__init__()
@@ -31,7 +30,7 @@ class GammaElevStacked(nn.Module):
         self.activation = torch.relu
         self.sigmoid = nn.Sigmoid()
 
-        self.encoder = Encoder(in_channels = in_channels+2)
+        self.encoder = Encoder(in_channels = in_channels)
         self.mlp = MLP(in_channels = 128,
             out_channels = 3,
             hidden_channels = 64,
@@ -42,47 +41,33 @@ class GammaElevStacked(nn.Module):
             n_params = 3
         )
 
-        self.elev_mlp = MLP(8,
+        self.elev_mlp = MLP(6,
             out_channels = 3,
             hidden_channels = 64,
             hidden_layers = 4)
-        
-        self.tmax_model = tmax_model
-        
+                
     def forward(self, h, mask, dists, elev):
-
-        self.tmax_model.eval()
-        
-        # Predict tmax at the target points
-        tmax_target, tmax_context  = tmax_model(h,
-                                       mask,
-                                       dists,
-                                       elev)
-        
-        # Make new context and mask 
-        mask = torch.cat([mask, torch.ones(mask.shape[0], 2, 87, 50).cuda()], dim = 1)
-        h = torch.cat([h, tmax_context.permute(0, 3, 1, 2)], axis = 1)
 
         # Encode with set convolution
         h = self.activation(self.encoder(h, mask))
         # Decode with CNN
         h = self.activation(self.decoder(h))
         # MLP 
-        h = self.mlp(h)
+        x = self.mlp(h)
         # out layer
-        rho, alpha, beta = self.out_layer(h, dists)
+        rho, alpha, beta = self.out_layer(x, dists)
         out = torch.cat([rho.view(*rho.shape, 1),
             alpha.view(*alpha.shape, 1),
             beta.view(*beta.shape, 1)], dim = 2)
 
         # Do elevation
         elev = elev.repeat(out.shape[0], 1, 1)
-        out = torch.cat([tmax_target, out[...,0], elev], dim = 2)
+        out = torch.cat([out[...,0], elev], dim = 2)
         out = self.elev_mlp(out)
         out[...,0] = self.sigmoid(out[...,0])
         out[...,1:] = force_positive(out[...,1:])
         
-        return out
+        return out, x
         
 class TmaxElevStacked(nn.Module):
     """
@@ -91,18 +76,18 @@ class TmaxElevStacked(nn.Module):
     Parameters:
     ----------
     decoder: convolutional architecture
-    
     in_channels: Int
         Number of context variables
     ls: float
         Initial length scale for the RBF kernel
+    precip_model: trained model to predict precipitation
     """
     
-    def __init__(self, decoder, in_channels=1, ls = 0.1):
+    def __init__(self, decoder, precip_model, in_channels=1, ls = 0.1):
         
         super().__init__()
 
-        self.encoder = Encoder(in_channels)
+        self.encoder = Encoder(in_channels+3)
 
         self.decoder= decoder
 
@@ -110,15 +95,28 @@ class TmaxElevStacked(nn.Module):
             hidden_channels = 64,
             hidden_layers = 4)
 
-        self.elev_mlp = MLP(5, 2,
+        self.elev_mlp = MLP(8, 2,
             hidden_channels = 64,
             hidden_layers = 4)
 
         self.activation_function = torch.relu
         self.final_layer = GaussianFinalLayer(0.1, 2)
+        self.precip_model = precip_model
  
 
     def forward(self, x, mask, dists, elev):
+
+        self.precip_model.eval()
+        
+        # Predict tmax at the target points
+        precip_target, precip_context  = precip_model(x,
+                                       mask,
+                                       dists,
+                                       elev)
+        
+        # Make new context and mask 
+        mask = torch.cat([mask, torch.ones(mask.shape[0], 3, 87, 50).cuda()], dim = 1)
+        x = torch.cat([x, precip_context.permute(0, 3, 1, 2)], axis = 1)
 
         # Encode with set convolution
         x = self.encoder(x, mask)
@@ -136,10 +134,11 @@ class TmaxElevStacked(nn.Module):
 
         # Do elevation
         elev = elev.repeat(out.shape[0], 1, 1)
-        out = torch.cat([out, elev], dim = 2)
+        out = torch.cat([precip_target, out, elev], dim = 2)
         out = self.elev_mlp(out)
         out[...,1] = force_positive(out[...,1])
         
         # Return target refinements and coarse scale predictions
-        return out, x
+        return out
+
 
